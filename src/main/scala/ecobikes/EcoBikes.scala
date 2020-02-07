edf294afa8eb3997c1d3bbd8e9d01426165d7b35
Sergiy Dobrovolsky <serggoodwill@gmail.com>
2020-02-07 15:53:29 +0200

    feat: EcoBikes

--- src/main/scala/ecobikes/EcoBikes.scala
+++ src/main/scala/ecobikes/EcoBikes.scala
@@ -4,54 +4,67 @@ import java.nio.file.Paths
 import java.nio.file.StandardOpenOption.{APPEND, CREATE, WRITE}
 
 import akka.NotUsed
-import akka.stream.scaladsl.{Broadcast, FileIO, Flow, Framing, GraphDSL, Sink, Source}
-import akka.stream.{FlowShape, Graph, IOResult}
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Broadcast, FileIO, Flow, Framing, GraphDSL, Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, FlowShape, Graph, IOResult}
 import akka.util.ByteString
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
 object EcoBikes extends App {
 
-  type FlowLike = Graph[FlowShape[ByteString, ByteString], NotUsed]
+  type FlowLike = Graph[FlowShape[String, ByteString], NotUsed]
 
   def processBikes(): FlowLike = {
-    val jsFlow = LogJson.jsonOutFlow
     Flow.fromGraph(
       GraphDSL.create() { implicit builder =>
         import GraphDSL.Implicits._
 
-        val delimiterFlow: Flow[ByteString, ByteString, NotUsed] =
-          Framing
-            .delimiter(ByteString("\n"), 1024)
-            .map(_.decodeString("UTF8"))
-            .map(s => ByteString(s))
-        val bcast = builder.add(Broadcast[ByteString](3))
-        val js = builder.add(jsFlow)
-        val electric = Flow[ByteString].filter(_.contains("E-BIKE"))
-        val folding = Flow[ByteString].filter(_.contains("FOLDING BIKE"))
-        val speedelec = Flow[ByteString].filter(_.contains("SPEEDELEC"))
+        val bsFlow: Flow[String, ByteString, NotUsed] =
+          Flow[String].map(s => ByteString(s))
 
-        source ~> delimiterFlow ~> bcast ~> electric ~> sink1
-        bcast ~> folding ~> sink2
-        bcast ~> speedelec ~> sink3
+        val format: Flow[String, String, NotUsed] =
+          Flow[String].map(s => s + '\n')
 
-        FlowShape(bcast.in, js.out)
+        val bcast = builder.add(Broadcast[String](4))
+        val bs = builder.add(bsFlow)
+        val electric = Flow[String].filter(_.contains("E-BIKE"))
+        val folding = Flow[String].filter(_.contains("FOLDING BIKE"))
+        val speedelec = Flow[String].filter(_.contains("SPEEDELEC"))
+
+        bcast ~> bs.in
+        bcast ~> electric ~> format ~> bsFlow ~> sinkBike("E-BIKE")
+        bcast ~> folding ~> format ~> bsFlow ~> sinkBike("FOLDING BIKE")
+        bcast ~> speedelec ~> bsFlow ~> sinkBike("SPEEDELEC")
+
+        FlowShape(bcast.in, bs.out)
       }
     )
   }
 
+
   val inputFile = Paths.get("/home/goodwill/Downloads/akka/ecobike.txt")
-
-  val outputFile1 = Paths.get("/home/goodwill/Downloads/akka/out_filter1.txt")
-
-  val outputFile2 = Paths.get("/home/goodwill/Downloads/akka/out_filter2.txt")
-
-  val outputFile3 = Paths.get("/home/goodwill/Downloads/akka/out_filter3.txt")
 
   val source: Source[ByteString, Future[IOResult]] = FileIO.fromPath(inputFile)
 
-  val sink1: Sink[ByteString, Future[IOResult]] = FileIO.toPath(outputFile1, Set(CREATE, WRITE, APPEND))
-  val sink2: Sink[ByteString, Future[IOResult]] = FileIO.toPath(outputFile2, Set(CREATE, WRITE, APPEND))
-  val sink3: Sink[ByteString, Future[IOResult]] = FileIO.toPath(outputFile3, Set(CREATE, WRITE, APPEND))
-}
+  val frame: Flow[ByteString, String, NotUsed] =
+    Framing
+      .delimiter(ByteString("\n"), 1024)
+      .map(_.decodeString("UTF8"))
 
+  def sinkBike(bikeType: String): Sink[ByteString, Future[IOResult]] =
+    FileIO.toPath(Paths.get(s"/home/goodwill/Downloads/akka/out_$bikeType.txt"), Set(CREATE, WRITE, APPEND))
+
+  val outputFile: Sink[ByteString, Future[IOResult]] =
+    FileIO.toPath(Paths.get("/home/goodwill/Downloads/akka/out_full.txt"), Set(CREATE, WRITE, APPEND))
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  source
+    .via(frame)
+    .via(processBikes())
+    .toMat(outputFile)(Keep.right)
+    .run()
+}
